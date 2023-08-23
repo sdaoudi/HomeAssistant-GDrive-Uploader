09e5393d85f69a24a5dd2387428cc61caa7339e8
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2023-08-23 23:59:25 +0200

    add support subfolders

--- custom_components/gdrive_uploader/api.py
+++ custom_components/gdrive_uploader/api.py
@@ -18,7 +18,7 @@ class GDriveApi:
         gauth.ServiceAuth()
         self.drive = GoogleDrive(gauth)
 
-    def create_folder(self, parent_folder_id, subfolder_name):
+    def _create_folder(self, parent_folder_id, subfolder_name):
         _LOGGER.debug(f"Create folder {subfolder_name} in parent {parent_folder_id}")
         new_folder = self.drive.CreateFile(
             {
@@ -36,8 +36,67 @@ class GDriveApi:
         new_folder.Upload()
         return new_folder
 
+    # Fonction pour obtenir l'ID du sous-dossier s'il existe, sinon renvoie None
+    def _get_subfolder_id(self, parent_folder_id, subfolder_name):
+        query = f"'{parent_folder_id}' in parents and title = '{subfolder_name}' and mimeType = 'application/vnd.google-apps.folder' and trashed = false"
+        subfolder_list = self.drive.ListFile({'q': query}).GetList()
 
-    def find_resource_by_title(self, parent_directory_id, resource_title):
+        if subfolder_list:
+            return subfolder_list[0]['id']
+        else:
+            return None
+
+    # Création de l'arborescence de répertoires si elle n'existe pas
+    def _create_subfolders(self, root_folder_id, folder_names):
+        current_folder_id = root_folder_id
+
+        for folder_name in folder_names:
+            subfolder_id = self._get_subfolder_id(current_folder_id, folder_name)
+
+            if subfolder_id:
+                current_folder_id = subfolder_id
+            else:
+                new_folder = self._create_folder(current_folder_id, folder_name)
+                current_folder_id = new_folder["id"]
+
+        return current_folder_id
+
+    def upload_file(self, source_file_path, parent_dir_id, directory_name: str = ""):
+        _LOGGER.debug(f"source file path : {source_file_path}")
+        _LOGGER.debug(f"parent directory id : {parent_dir_id}")
+        _LOGGER.debug(f"directory name : {str(directory_name)}")
+        if (not os.path.exists(source_file_path)):
+            raise FileNotFoundError(f"The file with path {source_file_path} is not found.")
+
+        if directory_name != "":
+            folder_names_to_create = directory_name.split("/")
+            final_folder_id = self._create_subfolders(parent_dir_id, folder_names_to_create)
+        else:
+            final_folder_id = parent_dir_id
+            _LOGGER.debug("The parent directory is used as a target directory")
+
+        filename = os.path.basename(source_file_path)
+        if (self._resource_exists(final_folder_id, filename)):
+            raise FileExistsError(f"File already exists in Google Drive with {filename} name.")
+
+        file = self.drive.CreateFile(
+            {
+                "parents": [
+                    {
+                        "id": final_folder_id
+                    }
+                ]
+            }
+        )
+
+        file.SetContentFile(source_file_path)
+        file.Upload()
+        _LOGGER.debug(f"A new file is created with id: {file['id']}")
+
+        return file
+
+
+    def _find_resource_by_title(self, parent_directory_id, resource_title):
         resource_list = self.drive.ListFile(
             {'q': f"'{parent_directory_id}' in parents and trashed=false"}
         ).GetList()
@@ -47,7 +106,7 @@ class GDriveApi:
                 return resource
         raise FileNotFoundError(f"The resource with title {resource_title} is not found inside the folder with {parent_directory_id} id.")
 
-    def resource_exists(self, parent_directory_id, resource_title):
+    def _resource_exists(self, parent_directory_id, resource_title):
         resource_list = self.drive.ListFile({'q':  f"'{parent_directory_id}' in parents and trashed=false"}).GetList()
         for resource in resource_list:
             if(resource['title'] == resource_title):
@@ -57,46 +116,9 @@ class GDriveApi:
         _LOGGER.debug(f"resource {resource_title} not exists in parent {parent_directory_id}")
         return False
 
-    def upload_file(self, source_file_path, parent_dir_id, directory_name):
-        _LOGGER.debug(f"source file path : {source_file_path}")
-        _LOGGER.debug(f"parent directory id : {parent_dir_id}")
-        _LOGGER.debug(f"directory name : {str(directory_name)}")
-        if (not os.path.exists(source_file_path)):
-            raise FileNotFoundError(f"The file with path {source_file_path} is not found.")
-
-        if directory_name != "":
-            try:
-                target_directory = self.find_resource_by_title(parent_dir_id, str(directory_name))
-                _LOGGER.debug(f"target directory already exists with id : {target_directory['id']}")
-            except FileNotFoundError:
-                target_directory = self.create_folder(parent_dir_id, directory_name)
-                _LOGGER.debug(f"The target directory has just been created with id : {target_directory['id']}")
-        else:
-            target_directory = {"id": parent_dir_id}
-            _LOGGER.debug("The parent directory is used as a target directory")
-
-        filename = os.path.basename(source_file_path)
-        if (self.resource_exists(target_directory["id"], filename)):
-            raise FileExistsError(f"File already exists in Google Drive with {filename} name.")
-
-        file = self.drive.CreateFile(
-            {
-                "parents": [
-                    {
-                        "id": target_directory["id"]
-                    }
-                ]
-            }
-        )
-
-        file.SetContentFile(source_file_path)
-        file.Upload()
-        _LOGGER.debug(f"A new file is created with id: {file['id']}")
-        return file
-
     def delete_directory_by_name(self, parent_dir_id, directory_name):
         try:
-            resource = self.find_resource_by_title(parent_dir_id, directory_name)
+            resource = self._find_resource_by_title(parent_dir_id, directory_name)
             self._delete_resource(resource["id"])
             resource.Delete()
         except FileNotFoundError:
